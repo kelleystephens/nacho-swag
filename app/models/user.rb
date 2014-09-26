5fba2852462bce6e041747b99d0e034a1c0a7659
Kelley Stephens <kelley.stephens@outlook.com>
2014-09-26 00:48:45 -0500

    styling added

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,14 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.username = auth.info.username
       user.bio = auth.info.bio
-      user.hometown = auth.info.hometown
+      user.hometown = auth.info.default_location
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.interests = auth.info.interests
       if auth.info.images
-        user.image = auth.info.images.image.first.url
+        user.image = auth.info.images.image.first.sticker280.url
+      else
+        user.image = "https://openclipart.org/image/800px/svg_to_png/192758/1397379218.png"
       end
     end
   end
